5f0756333cc397bb54f01ae29e8eddedaa3a5313
olivier.fous@gmail.com <olivier.fous@gmail.com>
2025-11-04 15:19:25 +0000

    V5

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -1,11 +1,10 @@
-    SELECT
-        orders_id,
-        date_date,
-        ROUND(SUM(revenue),2) AS revenue,
-        SUM(quantity) AS quantity
-        ROUND(SUM(quantity * purchase_price),2) AS purchase_cost,
-        ROUND(SUM(revenue - (quantity * purchase_price)),2) AS margin,
-    FROM {{ ref('int_sales_margin') }}
-    WHERE date_date = '2021-09-30'
-    AND orders_id IN(1002561, 1002560, 1002559)
-    GROUP BY orders_id, date_date
+SELECT
+    orders_id,
+    date_date,
+    ROUND(SUM(revenue),2) AS revenue,
+    ROUND(SUM(quantity),2) AS quantity,
+    ROUND(SUM(purchase_cost),2) AS purchase_cost,
+    ROUND(SUM(margin),2) AS margin
+FROM {{ ref('int_sales_margin') }}
+GROUP BY date_date, orders_id
+ORDER BY date_date DESC
